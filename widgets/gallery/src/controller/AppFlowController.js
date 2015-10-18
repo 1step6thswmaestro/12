d22d09bcca57fa1547dd13e88e8438677b14d615
Seokju Na <seokmaTD@gmail.com>
2015-10-18 19:29:29 +0900

    Gallery 위젯 flow 추가

--- widgets/gallery/src/controller/AppFlowController.js
+++ widgets/gallery/src/controller/AppFlowController.js
@@ -1,6 +1,9 @@
 var FlowController = require('flowing-js').FlowController;
+var Constants = require('../constants/Constants');
 
 var AppFlowController = new FlowController();
+
+AppFlowController.addFlow(Constants.FlowID.LOAD_PICTURES);
 
 
 module.exports = AppFlowController;
