d927cb57da1a93e24b6363ae07055914dad4d471
Seokju Na <seokmaTD@gmail.com>
2015-10-21 16:39:07 +0900

    Detail 화면 기능 구현 중 #2

--- widgets/weather/src/components/WeatherDetail/WeatherDetail.js
+++ widgets/weather/src/components/WeatherDetail/WeatherDetail.js
@@ -1,12 +1,62 @@
+var AppFlowController = require('../../controller/AppFlowController');
+var Constants = require('../../constants/Constants');
+
 var DateSelector = require('./DateSelector');
 var TempGraph = require('./TempGraph');
+
+var DayWeatherDetail = require('./MainDetail/DayWeatherDetail');
+var DayWeatherHeader = require('./MainDetail/DayWeatherHeader');
+var SunAndMoon = require('./MainDetail/SunAndMoon');
+var WindAndPressure = require('./MainDetail/WindAndPressure');
+
+
+var countCallback = 0;
+
+function setDataOfIndex(index) {
+
+}
+
+function initializeDatas() {
+    if (countCallback == 0) {
+        countCallback++;
+        return;
+    }
+
+    //TempGraph.initGraph();
+    //DateSelector.initItems();
+
+    setDataOfIndex(0);
+
+    countCallback = 0;
+}
 
 
 var WeatherDetail = {
     initialize: function($) {
+        countCallback = 0;
+
         DateSelector.initialize($);
         TempGraph.initialize($);
-    }
+
+        DayWeatherDetail.initialize($);
+        DayWeatherHeader.initialize($);
+        SunAndMoon.initialize($);
+        WindAndPressure.initialize($);
+    },
+
+    subscribeForecastData: AppFlowController.addSubscribe(
+        Constants.FlowID.GET_FORECAST_DATA,
+        function() {
+            initializeDatas();
+        }
+    ),
+
+    subscribeSunMoonData: AppFlowController.addSubscribe(
+        Constants.FlowID.GET_SUN_MOON_DATA,
+        function() {
+            initializeDatas();
+        }
+    )
 };
 
 module.exports = WeatherDetail;
---
+++ widgets/weather/src/constants/Localize.js
@@ -0,0 +1,65 @@
+module.exports = {
+    DayWeatherDetail: {
+        'feel-like': {
+            EN: "Feels Like",
+            KR: "체감온도"
+        },
+        'humanity': {
+            EN: "Humanity",
+            KR: "습도"
+        },
+        'dew-point': {
+            EN: "Dew Point",
+            KR: "이슬점"
+        },
+        'precipitation': {
+            EN: "Prob. of precip.",
+            KR: "강수확률"
+        },
+        'precipMM': {
+            EN: "Amount of precip.",
+            KR: "강수량"
+        },
+        'snowCM': {
+            EN: "Amount of Snow",
+            KR: "적설량"
+        }
+    },
+
+    SunAdnMoon: {
+        'helper-day': {
+            EN: "Day",
+            KR: "낮"
+        },
+        'helper-twilight': {
+            EN: "Twilight",
+            KR: "박명"
+        },
+        'helper-night': {
+            EN: "Night",
+            KR: "밤"
+        }
+    },
+
+    WindAndPressure: {
+        'anemometer': {
+            EN: "Anemometer",
+            KR: "풍속계"
+        },
+        'barometer': {
+            EN: "Barometer",
+            KR: "기압계"
+        }
+    },
+
+    DateSelector: {
+        'today': {
+            EN: "TODAY",
+            KR: "오늘"
+        },
+        'tomorrow': {
+            EN: "TOMORROW",
+            KR: "내일"
+        }
+    }
+};
