c3d44ec5a0edde225099af6d72d32c0ec2c86129
JARVIS <JARVI@asdf.asdf>
2015-10-23 09:26:27 +0900

    Runtime Environment.
    Adjust NuGet Package to support Calendar Widget.

--- widgets/calendar/Calendar/MainWindow.xaml.cs
+++ widgets/calendar/Calendar/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 using Google.Apis.Auth.OAuth2;
@@ -19,7 +12,6 @@ using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
 
-using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
