d9c16e6fb5ecb8e81a75d5ce1659f7d1d775bdfd
Seokju Na <seokmaTD@gmail.com>
2015-11-02 19:28:49 +0900

    Gallery Widget Normal Screen Publishing

--- widgets/gallery/build/js/main.js
+++ widgets/gallery/build/js/main.js
@@ -2,6 +2,8 @@
 'use strict';
 
 $(document).ready(function() {
+    var liveTiles = $('#component-live-tiles');
 
+    liveTiles.find('.tile-item').not('.exclude').liveTile();
 });
 },{}]},{},[1]);
--- widgets/gallery/src/main.js
+++ widgets/gallery/src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
 $(document).ready(function() {
+    var liveTiles = $('#component-live-tiles');
 
+    liveTiles.find('.tile-item').not('.exclude').liveTile();
 });
