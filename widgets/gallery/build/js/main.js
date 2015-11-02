55598c28f63e78c0a8c6d62e27b846883148e920
Seokju Na <seokmaTD@gmail.com>
2015-11-02 18:39:59 +0900

    Gallery Widget Publishing #1

--- widgets/gallery/build/js/main.js
+++ widgets/gallery/build/js/main.js
@@ -3,14 +3,5 @@
 
 $(document).ready(function() {
 
-    $('#dsff').slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        slidesToShow: 5,
-        slidesToScroll: 2,
-        adaptiveHeight: true
-    });
-    $('#dsff').slick('slickGoTo', 0);
 });
 },{}]},{},[1]);
--- widgets/gallery/src/main.js
+++ widgets/gallery/src/main.js
@@ -2,13 +2,4 @@
 
 $(document).ready(function() {
 
-    $('#dsff').slick({
-        dots: false,
-        arrows: false,
-        infinite: false,
-        slidesToShow: 5,
-        slidesToScroll: 2,
-        adaptiveHeight: true
-    });
-    $('#dsff').slick('slickGoTo', 0);
 });
