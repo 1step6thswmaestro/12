529ecd000f0dd4a6a69cafdb6cb67af634e51395
SoHyunSeop <iodesme@gmail.com>
2015-11-18 15:01:20 +0900

    글로벌 예외 처리 적용

--- core/Lumino/App.xaml.cs
+++ core/Lumino/App.xaml.cs
@@ -13,5 +13,21 @@ namespace Lumino
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Dispatcher.UnhandledExceptionFilter += Dispatcher_UnhandledExceptionFilter;
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+        }
+
+        private void Dispatcher_UnhandledExceptionFilter(object sender, System.Windows.Threading.DispatcherUnhandledExceptionFilterEventArgs e)
+        {
+            e.RequestCatch = true;
+        }
+
+        private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            Console.WriteLine("Global Exception : " + e.Exception.Message);
+        }
     }
 }
